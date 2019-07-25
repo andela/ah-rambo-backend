c4ce7ab6a49555a41b816bf5fafe9e407bfa03a7
Abdulfatai Jimoh <abdulfatai360@gmail.com>
2019-07-25 19:53:07 +0100

    chore-[167190525]: setup api documentation
    - install swagger-ui-express package to serve api docs to static url
    - install yamljs package to load api docs yaml file
    - write api documentation for the request to get the app root url
    
    [Finishes #167190525]

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 import express, { json, urlencoded } from 'express';
 import logger from 'morgan';
 import { config } from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 config();
 
 const { log } = console;
-
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+const swaggerDoc = YAML.load(
+  path.join(__dirname, './docs/authors-haven-api.yml')
+);
+
 app.use(logger('dev'));
 app.use(json());
-app.use(urlencoded({ extended: false, }));
+app.use(urlencoded({ extended: false }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.get('/', (request, response) => {
   response.status(200).send('Welcome to Authors Haven ');
@@ -20,5 +27,7 @@ app.get('/', (request, response) => {
 app.use('*', (request, response) => {
   response.status(404).send('Not Found');
 });
+
 app.listen(PORT, () => log(`Server started on port ${PORT}`));
+
 export default app;
