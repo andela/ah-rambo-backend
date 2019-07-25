77bc767ca577bf7434a193324d31e499a5044606
Adekoreday <khord4eng@gmail.com>
2019-07-25 17:29:23 +0100

    chore-[167515607]:setup unit test
    setup unit test using mocha-chai
    
    [Finishes #167515607]

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 import express, { json, urlencoded } from 'express';
 import logger from 'morgan';
+import { config } from 'dotenv';
+
+config();
+
+const { log } = console;
+
+const PORT = process.env.PORT || 9000;
 
 const app = express();
 app.use(logger('dev'));
@@ -13,5 +20,5 @@ app.get('/', (request, response) => {
 app.use('*', (request, response) => {
   response.status(404).send('Not Found');
 });
-
+app.listen(PORT, () => log(`Server started on port ${PORT}`));
 export default app;
---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../server';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('SERVER TEST', () => {
+  it('its expected to return a string on default url path', async () => {
+    const response = await chai.request(server).get('/');
+    expect(response).to.have.status(200);
+  });
+
+  it('its expected to return 404 on not found request', async () => {
+    const response = await chai.request(server).get('/unknown');
+    expect(response).to.have.status(404);
+  });
+});
