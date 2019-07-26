665383a359082f63fa3618b4b4b19c735ee5beeb
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-07-26 13:20:33 +0100

    chore(database):setup database with sequelize ORM - create .sequelizerc file - initialize sequelize-cli for database - configure sequelize to use ES6 [Finishes #167190519]

---
+++ server/database/models/index.js
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import { config } from 'dotenv';
+import * as dbData from '../config/config';
+
+config();
+
+const base = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const db = {};
+const dbUrl = dbData[env];
+
+let sequelize;
+if (dbUrl.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbUrl.use_env_variable], dbUrl);
+} else {
+  sequelize = new Sequelize(dbUrl.database, dbUrl.user, dbUrl.password, dbUrl);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => (
+    file.indexOf('.') !== 0) && (file !== base) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,29 @@
 import express, { json, urlencoded } from 'express';
 import logger from 'morgan';
 import { config } from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+import Debug from 'debug';
+
 
 config();
 
-const { log } = console;
-
 const PORT = process.env.PORT || 9000;
 
+config();
+
+const debug = Debug('dev');
+
 const app = express();
+const swaggerDoc = YAML.load(
+  path.join(__dirname, './docs/authors-haven-api.yml')
+);
+
 app.use(logger('dev'));
 app.use(json());
-app.use(urlencoded({ extended: false, }));
+app.use(urlencoded({ extended: false }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.get('/', (request, response) => {
   response.status(200).send('Welcome to Authors Haven ');
@@ -20,5 +32,7 @@ app.get('/', (request, response) => {
 app.use('*', (request, response) => {
   response.status(404).send('Not Found');
 });
-app.listen(PORT, () => log(`Server started on port ${PORT}`));
+
+app.listen(PORT, () => debug(`Server started on port ${PORT}`));
+
 export default app;
