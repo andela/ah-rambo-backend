1d92ba46b8f3bdbe95362b98adc8bac31a604a38
ayodejiAA <afolabiayodejia@gmail.com>
2019-07-31 15:47:42 +0100

    chore-[167619168]: Create sessions table - Create migration from session table - Add demo seed file for session details
    [Finishes #167619168]

---
+++ server/database/seeds/20190731140725-demo-session.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
+    'Sessions',
+    [
+      {
+        userId: 1,
+        active: true,
+        devicePlatform: 'browser',
+        expiresAt: new Date('2020-10-10'),
+        ipAddress: '121.234.188.49',
+        token:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+        userAgent:
+            'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 2,
+        active: true,
+        devicePlatform: 'browser',
+        expiresAt: new Date('2020-10-10'),
+        ipAddress: '121.234.200.49',
+        token:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+        userAgent:
+            'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ],
+    {}
+  ),
+  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Sessions', null, {})
+};
